1b9efd5f80dd6c1511a9db2e0c48c5965fa7a6f2
Christian Riedel <christian.riedel@siegwerk.com>
2022-08-24 08:51:54 +0200

    1.2.0b3

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['poetry==1.2.0b2'],
+    install_requires=['poetry==1.2.0b3'],
 )
